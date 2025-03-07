daf0b7e731b9e1e50a562c371e832a956eaad22e
Kah Kit <kahkitzheng@gmail.com>
2025-03-07 12:00:03 +0100

    Fix: Increase indent on file category

--- src/components/FileCategory.tsx
+++ src/components/FileCategory.tsx
@@ -24,7 +24,7 @@ export default function FileCategory(props: Readonly<FileCategoryProps>) {
 
   return (
     <>
-      <div className="grid gap-2 rounded-lg bg-neutral-100 p-4">
+      <div className="ml-2 grid gap-2 rounded-lg bg-neutral-100 px-4 py-2">
         <div className="flex justify-between">
           <div
             className="flex cursor-pointer items-center gap-2"

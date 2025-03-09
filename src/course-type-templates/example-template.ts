07210acbf046878a99271520400a7779ff20fd06
Kah Kit <kahkitzheng@gmail.com>
2025-03-09 14:14:30 +0100

    Feat: Add example template for course type files

---
+++ src/course-type-templates/example-template.ts
@@ -0,0 +1,41 @@
+import { FILE_NAME } from "../constants/file-name";
+import { FILE_SUBGROUP } from "../constants/file-subgroup";
+
+const TEACHER_MATERIALS: CourseTypeTemplate[] = [
+  {
+    name: FILE_NAME.ANTWOORDEN_DOEBOEK,
+    category: { course: true, chapter: false, tile: false },
+    files: [],
+  },
+];
+
+const TEACHING_MATERIALS: CourseTypeTemplate[] = [
+  {
+    name: FILE_NAME.ANTWOORDEN_DOEBOEK,
+    category: { course: true, chapter: false, tile: false },
+    files: [],
+  },
+];
+
+const EXTRA_TEACHING_MATERIALS: CourseTypeTemplate[] = [
+  {
+    name: FILE_NAME.ANTWOORDEN_DOEBOEK,
+    category: { course: true, chapter: false, tile: false },
+    files: [],
+  },
+];
+
+const DIFFERENTIATION_MATERIALS: CourseTypeTemplate[] = [
+  {
+    name: FILE_NAME.ANTWOORDEN_DOEBOEK,
+    category: { course: true, chapter: false, tile: false },
+    files: [],
+  },
+];
+
+export const EXAMPLE_TEMPLATE = {
+  [FILE_SUBGROUP.LEERKRACHTENMATERIAAL]: TEACHER_MATERIALS,
+  [FILE_SUBGROUP.LESMATERIAAL]: TEACHING_MATERIALS,
+  [FILE_SUBGROUP.EXTRA_LESMATERIAAL]: EXTRA_TEACHING_MATERIALS,
+  [FILE_SUBGROUP.DIFFERENTIATIEMATERIAAL]: DIFFERENTIATION_MATERIALS,
+};

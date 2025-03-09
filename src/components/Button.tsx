e1b185cb1838e37097e6bd73ebb141af179d1404
Kah Kit <kahkitzheng@gmail.com>
2025-03-09 14:14:15 +0100

    Fix: Change button bg color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export default function Button(props: Readonly<ButtonProps>) {
 
   return (
     <button
-      className="cursor-pointer rounded-lg border border-neutral-200 bg-white px-3 py-2 text-xs font-medium hover:bg-neutral-900 hover:text-neutral-100"
+      className="cursor-pointer rounded-lg border border-neutral-200 bg-neutral-900 px-3 py-2 text-xs font-semibold text-neutral-100 hover:bg-neutral-600"
       onClick={onClick}
       disabled={disabled}
     >
